6dca0c4bdcb908e1dc5b51faae3698224e9ac33d
[Jay Huang] <53930607+w286432531@users.noreply.github.com>
2022-02-07 01:09:40 -0500

    set up home page and product detail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-
+import React from "react";
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductDetailScreen from './screens/ProductDetailScreen';
 const App = () => {
   return (
-    <div className="App">
-    <h1>Welcome to the shop</h1>
-    </div>
+    <Router className="App">
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen}  exact/>
+          <Route path="/product/:id" component={ProductDetailScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Store</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart" />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user" />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+            {/* <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
