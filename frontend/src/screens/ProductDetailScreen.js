ab07ce8fa82fe7bc5e3bb6de8c6b68720a64d80d
[Jay Huang] <53930607+w286432531@users.noreply.github.com>
2022-02-08 02:20:11 -0500

    set up backend with express

--- frontend/src/screens/ProductDetailScreen.js
+++ frontend/src/screens/ProductDetailScreen.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
-
+import axios from 'axios';
 // match comes from prop
 const ProductDetailScreen = ({ match }) => {
   //params.id comes from link
-  const product = products.find((p) => p._id === match.params.id);
+  console.log(match)
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
   return (
     <div>
       <Link className="btn btn-light my-3" to="/">
